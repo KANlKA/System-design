aed7d4cb33c0dadb350a08108691efc0c0597c70
kanika <155656637+KANlKA@users.noreply.github.com>
2026-07-11 04:06:41 +0530

    Update Consistent Hashing.java

--- algorithms/Consistent Hashing.java
+++ algorithms/Consistent Hashing.java
@@ -11,15 +11,15 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Function;
 
 public class ConsistentHashing implements Router {
-    private final Map<Node, List<Long>> nodePositions;
-    private final ConcurrentSkipListMap<Long, Node> nodeMappings;
-    private final Function<String, Long> hashFunction;
-    private final int pointMultiplier;
-
+    private final Map<Node, List<Long>> nodePositions;//list of all virtual node positions so we know when a node is added or removed, the exact pos it was associated with
+    private final ConcurrentSkipListMap<Long, Node> nodeMappings;//ConcurrentSkipListMap automatically keeps keys sorted because consistent hashing always searches for the next greater hash value
+    private final Function<String, Long> hashFunction;//Instead of hardcoding MD5/SHA-1, the constructor accepts any hash function
+    private final int pointMultiplier;//One physical node can appear many times on the ring. These are called virtual nodes
+//MORE VIRTUAL NODES=BETTER LOAD BALANCING
 
     public ConsistentHashing(final Function<String, Long> hashFunction,
                              final int pointMultiplier) {
-        if (pointMultiplier == 0) {
+        if (pointMultiplier == 0) { //IF NO VIRTUAL NODES->Initializes the below
             throw new IllegalArgumentException();
         }
         this.pointMultiplier = pointMultiplier;
@@ -29,12 +29,12 @@ public class ConsistentHashing implements Router {
     }
 
     public void addNode(Node node) {
-        nodePositions.put(node, new CopyOnWriteArrayList<>());
-        for (int i = 0; i < pointMultiplier; i++) {
-            for (int j = 0; j < node.getWeight(); j++) {
+        nodePositions.put(node, new CopyOnWriteArrayList<>());//first creates and empty list
+        for (int i = 0; i < pointMultiplier; i++) {//if multiplier is 3 then each iteration creates a virtual node
+            for (int j = 0; j < node.getWeight(); j++) {//weight controls how many pos each virtual node gets. High wt->more entry->more traffic
                 final var point = hashFunction.apply((i * pointMultiplier + j) + node.getId());
-                nodePositions.get(node).add(point);
-                nodeMappings.put(point, node);
+                nodePositions.get(node).add(point);//saving pos
+                nodeMappings.put(point, node);//store in ring
             }
         }
     }
@@ -44,11 +44,11 @@ public class ConsistentHashing implements Router {
             nodeMappings.remove(point);
         }
     }
-
+//Now Node A disappears from the ring. Only requests previously mapped near those positions will move to another node. This is the key advantage of consistent hashing: removing a node only remaps a small portion of the keys instead of all of them.
     public Node getAssignedNode(Request request) {
         final var key = hashFunction.apply(request.getId());
-        final var entry = nodeMappings.higherEntry(key);
-        if (entry == null) {
+        final var entry = nodeMappings.higherEntry(key);//next server find 
+        if (entry == null) {//in this case suppose the last server is 9000 and your hash is 9800 so theres no larger val from that, so there will be a wrap around where this hash will go to the 1st server as its a ring
             return nodeMappings.firstEntry().getValue();
         } else {
             return entry.getValue();
