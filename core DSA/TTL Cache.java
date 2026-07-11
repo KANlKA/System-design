b3e679fd1f07eab8e319db52a0cddc4c44281f44
kanika <155656637+KANlKA@users.noreply.github.com>
2026-07-11 17:47:57 +0530

    Update and rename TTL cache.java to TTL Cache.java

--- core DSA/TTL Cache.java
+++ core DSA/TTL Cache.java
@@ -1,49 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
-
+//A TTL (Time-To-Live) Cache stores data only for a fixed amount of time. Once the TTL expires, the entry is treated as invalid and is removed (or ignored).
 class TTLCache {
-
     class CacheEntry {
-
         int value;
         long expiryTime;
-
         CacheEntry(int value, long expiryTime) {
             this.value = value;
             this.expiryTime = expiryTime;
         }
     }
-
     private final Map<Integer, CacheEntry> cache;
-
     public TTLCache() {
         cache = new HashMap<>();
     }
-
     public void put(int key, int value, long ttlMillis) {
-
         long expiry = System.currentTimeMillis() + ttlMillis;
-
         cache.put(key, new CacheEntry(value, expiry));
     }
-
     public int get(int key) {
-
         if (!cache.containsKey(key))
             return -1;
-
         CacheEntry entry = cache.get(key);
-
         if (System.currentTimeMillis() > entry.expiryTime) {
-
             cache.remove(key);
-
             return -1;
         }
-
         return entry.value;
     }
-
     public void remove(int key) {
         cache.remove(key);
     }
